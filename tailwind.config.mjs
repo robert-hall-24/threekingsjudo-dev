ba694641e76f583cd72d96de9135e2e12d87d967
roberthall <rhall477@gmail.com>
2024-12-28 18:48:58 +1300

    skele for website homepage ready

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'logo-color': '#B49C78',
+        'dark-color-logo': '#8d7e66',
+      },
+      fontFamily: {
+        righteous: ['Righteous', 'cursive'],
+        kosugi: ['Kosugi Maru', 'sans-serif'],
+        fahkwang: ['Fahkwang', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
 }
